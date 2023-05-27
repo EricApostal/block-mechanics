5473592b301dca63ae723b8adaddcf3552db1822
Eric Apostal <ehapostal04@gmail.com>
2023-05-26 22:53:56 -0400

    changed to collectionservice

--- src/client/game/BlockMechanics.lua
+++ src/client/game/BlockMechanics.lua
@@ -1,20 +1,19 @@
 local BlockMechanics = {}
 
 local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
 
 local player = Players.LocalPlayer -- 17.268
 
 local mouse = player:GetMouse()
 
-local function placeBlock(position)
-    local block = Instance.new("Part")
-    block.Size = Vector3.new(3,3,3)
+local function placeBlock(position, type)
+    local block = ReplicatedStorage.blocks:WaitForChild(type):Clone()
+    CollectionService:AddTag(block, "block")
     block.Parent = workspace.blocks
-    block.Anchored = true
-    block.Name = "block"
     block.Position = position
-    block.Material = "Grass"
-    block.Color = Color3.fromRGB(26,165,16)
+    block.Anchored = true
 end
 
 local lastClickUp = true
@@ -27,27 +26,27 @@ local function BuildChunk(startX, startZ)
         for z = (startZ*16)*3, chunkSize*3 + (startZ*16)*3, 3  do
             local y = ((1+math.noise(x/scale, z/scale, seed/1000))/2)
             local min, max = 0, scale/2
-            placeBlock( Vector3.new(x, math.round( (min+(max-min)*y)/3)*3, z) )
+            placeBlock( Vector3.new(x, math.round( (min+(max-min)*y)/3)*3, z), "grass" )
         end
     end
 end
 
 local function handleBreaking()
     mouse.Button2Down:Connect(function()
-        if (not lastClickUp) or not (mouse.Target) or not (mouse.Target.Name == "block") then return end
+        if (not lastClickUp) or not (mouse.Target) or not (CollectionService:HasTag(mouse.Target, "block") ) then return end
         
         if mouse.TargetSurface.Name == "Right" then
-            placeBlock( Vector3.new(mouse.Target.Position.X+3, mouse.Target.Position.Y, mouse.Target.Position.Z) )
+            placeBlock( Vector3.new(mouse.Target.Position.X+3, mouse.Target.Position.Y, mouse.Target.Position.Z), "oak_log" )
         elseif mouse.TargetSurface.Name == "Top" then 
-            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y + 3, mouse.Target.Position.Z) )
+            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y + 3, mouse.Target.Position.Z), "oak_log" )
         elseif mouse.TargetSurface.Name == "Left" then
-            placeBlock( Vector3.new(mouse.Target.Position.X-3, mouse.Target.Position.Y, mouse.Target.Position.Z) )
+            placeBlock( Vector3.new(mouse.Target.Position.X-3, mouse.Target.Position.Y, mouse.Target.Position.Z), "oak_log" )
         elseif mouse.TargetSurface.Name == "Bottom" then
-            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y - 3, mouse.Target.Position.Z) )
+            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y - 3, mouse.Target.Position.Z), "oak_log" )
         elseif mouse.TargetSurface.Name == "Front" then
-            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y, mouse.Target.Position.Z-3) )
+            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y, mouse.Target.Position.Z-3), "oak_log" )
         elseif mouse.TargetSurface.Name == "Back" then
-            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y, mouse.Target.Position.Z+3) )
+            placeBlock( Vector3.new(mouse.Target.Position.X, mouse.Target.Position.Y, mouse.Target.Position.Z+3), "oak_log" )
         end
     end)
     mouse.Button2Up:Connect(function()
@@ -62,10 +61,11 @@ end
 local function buildChunks()
     coroutine.wrap(function()
         BuildChunk(0,0)
-        for x = -10, 10 do
-            for y = -10, 10 do
+        for x = -5, 5 do
+            for y = -5, 5 do
                 BuildChunk(x,y)
             end
+            task.wait()
         end
     end)()
 end
