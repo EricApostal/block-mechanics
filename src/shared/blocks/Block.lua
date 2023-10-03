b6542e2ef50c66525e008260bfb9301ce8b80649
Eric Apostal <ehapostal04@gmail.com>
2023-10-03 11:17:01 -0400

    Fixed, I was setting attributes wrong

--- src/shared/blocks/Block.lua
+++ src/shared/blocks/Block.lua
@@ -4,24 +4,18 @@
 
 local BlockMap = require(script.Parent.Parent.BlockMap)
 
-Block = {
-    position = Vector3.new(0, 0, 0),
-    texture = "stone",
-    breakTimes = {
-        hand = 3
-    }
-}
+local Block = {} -- Create an empty table for the Block class
 
--- Makes a new block.
-function Block:new(position: Vector3, texture: string, breakTimes: number)
-    local obj = {}
+-- Constructor for a new block.
+function Block:new(position, texture, breakTimes)
+    local obj = {} -- Create a new table for the block instance
     setmetatable(obj, self)
     self.__index = self
-    self.position = position
-    self.texture = texture
-    self.breakTimes = breakTimes
+    obj.position = position
+    obj.texture = texture
+    obj.breakTimes = breakTimes
 
-    if (texture == nil) then
+    if texture == nil then
         error("ERROR: Block was created but no texture was defined.")
     end
 
@@ -29,28 +23,30 @@ function Block:new(position: Vector3, texture: string, breakTimes: number)
 end
 
 -- Move block by converting Roblox coordinates to Voxel coordinates.
-function Block:moveTo(position: Vector3)
+function Block:moveTo(position)
     -- This may just not work
     local robloxPosition = BlockMap:getPos(position)
+    -- Update the instance-specific position
+    self.position = robloxPosition
 end
 
-function Block:getChunk(): Vector2
+function Block:getChunk()
     return BlockMap:getChunk(self.position)
 end
 
 -- Get the X,Y,Z hash of the block
-function Block:getHash(): string
+function Block:getHash()
     return string.format("%s,%s,%s", self.position.X, self.position.Y, self.position.Z)
 end
 
 -- Get the hash of the current chunk
-function Block:getChunkHash():string
+function Block:getChunkHash()
     return string.format("%s,%s", self:getChunk().X, self:getChunk().Y)
 end
 
 -- Serialize so we can pass over the network.
 function Block:serialize()
-    return {self.position, self.texture, self.breakTimes, self.chunk}
+    return { self.position, self.texture, self.breakTimes, self:getChunk() }
 end
 
 return Block
