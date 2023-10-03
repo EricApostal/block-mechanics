509aa689d7ed716cf597e81b0491dde5baa1a0ea
Eric Apostal <ehapostal04@gmail.com>
2023-10-03 11:59:51 -0400

    fixed getChunk to work with both chunks & blocks, WIP breaking

--- src/shared/BlockMap.lua
+++ src/shared/BlockMap.lua
@@ -13,16 +13,41 @@ function blockMap:VoxelToRBX(block)
     return Vector3.new( math.round(block.X*3), math.round(block.Y*3), math.round(block.Z*3) )
 end
 
-function blockMap:getChunk(chunkPos: Vector2)
-    -- TODO: Handle if a Vector2 is passed
-    local pos = {X = math.round( ((chunkPos.X/3) -8 )/16) , Y = math.round( ((chunkPos.Y/3) -8 )/16)}
-    if pos.X == -0 then
-        pos.X = 0
+function blockMap:getChunk(position)
+    local pos
+
+    if (typeof(position) == "Vector2") then
+        -- Are we getting the postition of a chunk?
+       pos = {X = math.round( ((position.X/3) -8 )/16) , Y = math.round( ((position.Y/3) -8 )/16)}
+        if pos.X == -0 then
+            pos.X = 0
+        end
+
+        if pos.Y == -0 then
+            pos.Y = 0
+        end
+
+        return Vector2.new(pos.X, pos.Y)
+        
+    elseif(typeof(position) == "Vector3") then
+        -- Are we getting the position of a block?
+        pos = {X = math.round( ((position.X/3) -8 )/16) , Z = math.round( ((position.Z/3) -8 )/16)}
+        if pos.X == -0 then
+            pos.X = 0
+        end
+
+        if pos.Y == -0 then
+            pos.Y = 0
+        end
+
+        if pos.Z == -0 then
+            pos.Z = 0
+        end
+        return Vector2.new(pos.X, pos.Z)
+    else
+        error("Tried to pass something other than a Vector2 / Vector3!")
+        return nil
     end
-    if pos.Y == -0 then
-        pos.Y = 0
-    end
-    return Vector2.new(pos.X, pos.Y)
 end
 
 function blockMap:toHash(chunkPos: Vector3)
